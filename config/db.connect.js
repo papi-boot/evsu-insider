80d19ab47ed057b0e2845679eac4166e99012029
papi-boot <papi8261@gmail.com>
2021-06-07 04:42:03 +0800

    config database connection again

--- config/db.connect.js
+++ config/db.connect.js
@@ -3,12 +3,13 @@ require("dotenv").config({ path: __dirname + "/.env" });
 const { Sequelize, DataTypes, QueryTypes } = require("sequelize");
 
 let connectionStringOption = {};
+let connectionString;
 const isProduction = process.env.NODE_ENV === "production";
 
 //Check if we are on production mode or development mode
 if (isProduction) {
+  connectionString = process.env.DATABASE_URL;
   connectionStringOption = {
-    connectionString: process.env.DATABASE_URL,
     dialect: "postgres",
     dialectOptions: {
       ssl: {
@@ -19,15 +20,15 @@ if (isProduction) {
     logging: false,
   };
 } else {
+  connectionString = `postgres://${process.env.LOCAL_DATABASE_URL}`;
   connectionStringOption = {
-    connectionString: `postgres://${process.env.LOCAL_DATABASE_URL}`,
     dialect: "postgres",
     logging: false,
   };
 }
 console.log(connectionStringOption);
 
-const sequelize = new Sequelize(connectionStringOption);
+const sequelize = new Sequelize(connectionString, connectionStringOption);
 
 try {
   sequelize
