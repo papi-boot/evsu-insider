d7b45a5fc30d29177766331f4d98ad190dbe2e2e
papi-boot <papi8261@gmail.com>
2021-06-08 10:46:34 +0800

    share answer switch to mobile theme

--- public/js/tinymce.form.js
+++ public/js/tinymce.form.js
@@ -8,7 +8,7 @@ window.addEventListener("load", () => {
     content_style:
       'body { font-family: "Palanquin",sans-serif; font-size: 0.9rem } pre { font-family: "Palanquin",sans-serif; font-size: 0.7rem } code[class*="language-"], pre[class*="language-"]{ } ',
     plugins:
-      "anchor code emoticons charmap wordcount  codesample lists advlist table hr pagebreak nonbreaking print image media imagetools autolink link preview fullscreen visualblocks spellchecker visualchars autolink paste help searchreplace",
+      "autosave anchor code emoticons charmap wordcount codesample lists advlist table hr pagebreak nonbreaking print image media imagetools autolink link preview fullscreen visualblocks spellchecker visualchars autolink paste help searchreplace",
     height: "80vh",
     toolbar:
       "insertfile undo redo | styleselect | bold italic forecolor backcolor fontsizeselect | alignleft aligncenter alignright alignjustify bullist numlist table outdent indent | image media codesample emoticons charmap link  | anchor hr pagebreak nonbreaking paste | print help addTab",
@@ -25,12 +25,27 @@ window.addEventListener("load", () => {
     object_resizing: true,
     //for Mobile
     mobile: {
-      theme: "silver",
+      theme: "mobile",
       menubar: true,
-      plugins:
-        "anchor emoticons charmap wordcount codesample lists advlist table hr pagebreak nonbreaking print autolink link preview fullscreen spellchecker autolink paste help",
-      toolbar:
-        "insertfile undo redo | styleselect | bold italic forecolor backcolor fontsizeselect | alignleft aligncenter alignright alignjustify bullist numlist table indent | codesample emoticons charmap link | anchor hr pagebreak | help",
+      // plugins:
+      //   "anchor emoticons charmap wordcount codesample lists advlist table hr pagebreak nonbreaking print autolink link preview fullscreen spellchecker autolink paste help",
+      // toolbar:
+      //   "insertfile undo redo | styleselect | bold italic forecolor backcolor fontsizeselect | alignleft aligncenter alignright alignjustify bullist numlist table indent | codesample emoticons charmap link | anchor hr pagebreak | help",
+      plugins: ["autosave", "lists", "autolink"],
+      toolbar: [
+        "undo",
+        "redo",
+        "bold",
+        "underline",
+        "italic",
+        "styleselect",
+        "forecolor",
+        "fontsizeselect",
+        "bullist",
+        "numlist",
+        "link",
+        "unlink"
+      ],
       toolbar_sticky: true,
       resize: true,
       object_resizing: true,
